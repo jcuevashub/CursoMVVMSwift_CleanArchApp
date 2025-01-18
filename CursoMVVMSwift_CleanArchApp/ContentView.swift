861ae65e63e51725bae542c076fc09d3376c2485
Jackson Cuevas <jackson.cuevas@hotmail.com>
2025-01-18 09:22:09 -0400

    Initial Commit

---
+++ CursoMVVMSwift_CleanArchApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursoMVVMSwift_CleanArchApp
+//
+//  Created by Jackson Cuevas on 18/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursoMVVMSwift_CleanArchApp/CursoMVVMSwift_CleanArchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoMVVMSwift_CleanArchAppApp.swift
+//  CursoMVVMSwift_CleanArchApp
+//
+//  Created by Jackson Cuevas on 18/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoMVVMSwift_CleanArchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
